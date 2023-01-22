cd27a44dccab49a9121ca204c02a951146d9367d
hieu.nq <ngohieu1811@gmail.com>
2023-01-22 22:47:52 +0700

    .

--- src/header.js
+++ src/header.js
@@ -49,6 +49,7 @@ const Header = (props) => {
           position: "fixed",
           top: 0,
           left: 0,
+          zIndex: -1,
           width: "100vw",
           height: "100vh",
         }}
